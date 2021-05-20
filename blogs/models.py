3fd17197ec2044c1e40d004c19f8be44922d2680
Khanh <tran_khanh@outlook.com>
2021-05-20 11:18:06 +0700

    update

--- blogs/models.py
+++ blogs/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_delete
-from django.dispatch import receiver
+from django.dispatch import reciver
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 
