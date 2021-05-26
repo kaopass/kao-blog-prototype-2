69bf38ffc1ec2cb088defc50c1560a52f412393c
Tran Khanh <tran_khanh@outlook.com>
2021-05-26 21:15:00 +0700

    implement django-dark

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+from django.utils.html import format_html
+from django.db.models import Count
+
+from .models import Blog
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    def subdomain_url(self, obj):
+        blog = Blog.objects.get(user=obj)
+        return format_html(
+            "<a href='{url}' target='_blank>{url}</a>",
+            url={blog.useful_domain()}
+        )
+    subdomain_url.short_description = "Subdomain"
+
+    list_display = ('email', 'subdomain_url', 'is_active', 'is_staff', 'date_joined')
+    ordering = ('-date_joined',)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
---
+++ packages/django-dark/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    author="Tran Khanh",
+    author_email="trkhanh8@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
